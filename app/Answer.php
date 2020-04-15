cc2a7a29e36fe00ff1567f68af9f79875985f278
abelic <androbelic@gmail.com>
2020-04-15 20:49:33 +0200

    Update best answer id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -29,12 +29,8 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-          $question = $answer->question;
-          $question->decrement('answers_count');
-          if($question->best_answer_id === $answer->id){
-            $question->best_answer_id =NULL;
-            $question->save();
-          }
+          $answer->question->decrement('answers_count');
+
         });
 
     }
