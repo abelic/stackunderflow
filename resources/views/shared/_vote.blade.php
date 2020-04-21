b0795777078c7b4fd7b92d2647b64bebd869bdf2
bajicsara <bajicsarait@gmail.com>
2020-04-21 22:40:00 +0200

    Tidy up the views

--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -1,23 +1,19 @@
-
 @if ($model instanceof App\Question)
-  @php
-
-    $name='question';
-    $firstURISegment='questions';
-
-  @endphp
-
-@elseif ($model instanceof App\Question)
-    $name='answer';
-    $firstURISegment='answers';
+    @php
+        $name = 'question';
+        $firstURISegment = 'questions';
+    @endphp
+@elseif ($model instanceof App\Answer)
+    @php
+        $name = 'answer';
+        $firstURISegment = 'answers';
+    @endphp
 @endif
 
 @php
-
-  $formId = $name . "-" . $model->id;
-  $formAction= "/{$firstURISegment}/{$model->id}/vote";
+    $formId = $name . "-" . $model->id;
+    $formAction = "/{$firstURISegment}/{$model->id}/vote";
 @endphp
-
 <div class="d-fex flex-column vote-controls">
     <a title="This {{ $name }} is useful"
         class="vote-up {{ Auth::guest() ? 'off' : '' }}"
@@ -25,7 +21,7 @@
         >
         <i class="fas fa-caret-up fa-3x"></i>
     </a>
-    <form id="up-vote-{{ $formId }}" action="/public/{{ $formAction }}/vote" method="POST" style="display:none;">
+    <form id="up-vote-{{ $formId }}" action="{{ $formAction }}" method="POST" style="display:none;">
         @csrf
         <input type="hidden" name="vote" value="1">
     </form>
@@ -38,18 +34,14 @@
         >
         <i class="fas fa-caret-down fa-3x"></i>
     </a>
-    <form id="down-vote-{{ $formId }}" action="/public/{{ $formAction }}/vote" method="POST" style="display:none;">
+    <form id="down-vote-{{ $formId }}" action="{{ $formAction }}" method="POST" style="display:none;">
         @csrf
         <input type="hidden" name="vote" value="-1">
     </form>
 
-    @if($model instanceof App\Question)
-      @include ('shared._favorite', [
-        'model'=>$model
-      ])
+    @if ($model instanceof App\Question)
+        <favorite :question="{{ $model }}"></favorite>
     @elseif ($model instanceof App\Answer)
-    @include ('shared._accept', [
-      'model'=>$model
-    ])
+        <accept :answer="{{ $model }}"></accept>
     @endif
 </div>
